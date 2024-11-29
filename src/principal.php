e3a6849ec695f327d3cfee2ecb174def5b8190f3
JoelQuia <oliver.joel2020@gmail.com>
2024-11-28 21:59:49 -0300

    principal

--- src/principal.php
+++ src/principal.php
@@ -15,7 +15,55 @@
 <?php
 include "Include/navbar.php";
 include "Include/main.php";
-include "Include/footer.php"
+?>
+
+<div class="bemvindo">
+    <h1>Bem Vindo ao <span>MarketRecycla!</span></h1>
+    <p>Uma empresa que conecta você à sustentabilidade ambiental e ao desenvolvimento de uma sociedade consciente com os cuidados da natureza</p>
+</div>
+
+<div class="paginas">
+  <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+ 
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="imagens/empresa.webp" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>O MarketRecycla é o futuro da Sustentabilidade</h5>
+        <p >Nosso empresa fornece uma estrutura satisfatória para o consumidor</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="imagens/importancia.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="comunidade.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden"></span>
+  </button>
+  </div>
+
+</div>
+
+
+
+<?php
+include "Include/footer.php";
 ?>
    
 
@@ -23,6 +71,7 @@ include "Include/footer.php"
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     
 </body>
 
